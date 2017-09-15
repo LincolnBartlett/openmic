939bf90cfa37162bdb2a2fb4254d72e7dfbda55d
LincolnBartlett <lincolnbartlett@gmail.com>
2017-09-14 17:16:52 -0700

    first upload

---
+++ index.js
@@ -0,0 +1,35 @@
+//DEPENDENCIES
+var express         = require('express'),
+    mongoose        = require('mongoose'),
+    ejs             = require('ejs'),
+    bodyParser      = require('body-parser'),
+    methodOverride  = require('method-override'),
+    dateFormat      = require('dateformat');
+
+//EXPRESS 
+var app = express();
+app.use(express.static(`${__dirname}/public`));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+    
+//DATABASE
+mongoose.connect('mongodb://localhost/openmic');
+
+//ROUTES
+app.get('/', function(req, res){
+    res.render('front')
+});
+
+
+var appRoute = require('./routes/app/app.js');
+
+app.use('/app', appRoute);
+
+//PORT
+var port = 3000;
+app.listen(port, function(){
+    console.log(`Server Started on ${dateFormat(new Date())}`)
+});
+
+
