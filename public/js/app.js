b65abc7262a51f7506175c3924d9157cf98ebb6f
LincolnBartlett <lincolnbartlett@gmail.com>
2017-10-07 20:13:24 -0700

    cleaned up

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,14 @@
-// app.js
+var app = angular.module('micMap', ['queryCtrl','venueAdd', 'geolocation', 'gservice', 'ngRoute'])
 
-// Declares the initial angular module "meanMapApp". Module grabs other controllers and services. Note the use of ngRoute.
-var app = angular.module('micMap', ['addCtrl', 'queryCtrl','venueAdd', 'geolocation', 'gservice', 'ngRoute'])
-
-    // Configures Angular routing -- showing the relevant view and controller when needed.
     .config(function($routeProvider){
 
-        // Join Team Control Panel
         $routeProvider.when('/find', {
             controller: 'queryCtrl',
             templateUrl: 'partials/queryForm.html',
 
-            // All else forward to the Join Team Control Panel
         }).when('/addvenue', {
             controller: 'venueAdd',
             templateUrl: 'partials/venueForm.html',
 
-            // All else forward to the Join Team Control Panel
         }).otherwise({redirectTo:'/find'});
-    });
+    }); 
--- public/js/queryCtrl.js
+++ public/js/queryCtrl.js
@@ -1,14 +1,11 @@
 var queryCtrl = angular.module('queryCtrl', ['geolocation', 'gservice']);
+
 queryCtrl.controller('queryCtrl', function($scope, $log, $http, $rootScope, geolocation, gservice){
 
-    // Initializes Variables
-    // ----------------------------------------------------------------------------
     $scope.value = 75;
     $scope.formData = {};
     var queryBody = {};
 
-    // Functions
-    // ----------------------------------------------------------------------------
     geolocation.getLocation().then(function(data){
             coords = {lat:data.coords.latitude, long:data.coords.longitude};
             $scope.formData.longitude = parseFloat(coords.long).toFixed(3);
@@ -17,38 +14,28 @@ queryCtrl.controller('queryCtrl', function($scope, $log, $http, $rootScope, geol
         
         });
 
-    // Get coordinates based on mouse click. When a click event is detected....
     $rootScope.$on("clicked", function(){
-        // Run the gservice functions associated with identifying coordinates
         $scope.$apply(function(){
             $scope.formData.latitude = parseFloat(gservice.clickLat).toFixed(3);
             $scope.formData.longitude = parseFloat(gservice.clickLong).toFixed(3);
         });
     });
 
-    // Take query parameters and incorporate into a JSON queryBody
     $scope.queryUsers = function(){
-
-        // Assemble Query Body
         queryBody = {
             longitude: parseFloat($scope.formData.longitude),
             latitude: parseFloat($scope.formData.latitude),
             distance: $scope.value
         };
-        console.log(queryBody);
-        // Post the queryBody to the /query POST route to retrieve the filtered results
-        $http.post('/query', queryBody)
-            // Store the filtered results in queryResults
-            .success(function(queryResults){
-                // Query Body and Result Logging
-                // Pass the filtered results to the Google Map Service and refresh the map
-                gservice.refresh(queryBody.latitude, queryBody.longitude, queryResults);
-                // Count the number of records retrieved for the panel-footer
-                $scope.queryResults = queryResults;
-                $scope.queryCount = queryResults.length;
-            })
-            .error(function(queryResults){
-                console.log('Error ' + queryResults);
-            })
+
+    $http.post('/query', queryBody)
+        .success(function(queryResults){
+            gservice.refresh(queryBody.latitude, queryBody.longitude, queryResults);
+            $scope.queryResults = queryResults;
+            $scope.queryCount = queryResults.length;
+        })
+        .error(function(queryResults){
+            console.log('Error ' + queryResults);
+        })
     };
 });
